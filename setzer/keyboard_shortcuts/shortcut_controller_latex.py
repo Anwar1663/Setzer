6b52547e57255dd121781185d352e5a5daefe87e
Anwar <anwarbediaf.cr@gmail.com>
2024-07-24 11:48:02 +0100

    Updated keyboard shortcuts

--- setzer/keyboard_shortcuts/shortcut_controller_latex.py
+++ setzer/keyboard_shortcuts/shortcut_controller_latex.py
@@ -38,31 +38,30 @@ class ShortcutControllerLaTeX(ShortcutController):
         self.set_accels_for_insert_before_after_action(['\\textbf{', '}'], ['<Control>b'])
         self.set_accels_for_insert_before_after_action(['\\textit{', '}'], ['<Control>i'])
         self.set_accels_for_insert_before_after_action(['\\underline{', '}'], ['<Control>u'])
-        self.set_accels_for_insert_before_after_action(['\\texttt{', '}'], ['<Control><Shift>t'])
+        self.set_accels_for_insert_before_after_action(['\\texttt{', '}'], ['<Control>t'])
         self.set_accels_for_insert_before_after_action(['\\emph{', '}'], ['<Control><Shift>e'])
 
 
         self.set_accels_for_insert_before_after_action(['\\begin{gather*}\n\t ','\n\\end{gather*}'], ['<Control>m'])
-        self.set_accels_for_insert_before_after_action(['\\begin{matrix}\n\t ','\n\\end{matrix}'], ['<Control><Shift>m'])
+        self.set_accels_for_insert_before_after_action(['\\left [ \\ \\begin{matrix}\n\t ','\\\\\n\t•\n\\end{matrix} \\ \\right ]'], ['<Control><Shift>m'])
         self.set_accels_for_insert_before_after_action(['\\begin{equation}\n\t\\begin{split}\n\t eq','\n\t\n\\end{equation}'], ['<Control><Shift>n'])
         self.set_accels_for_insert_before_after_action(['\\begin{•}\n\t', '\n\\end{•}'], ['<Control>e'])
         self.set_accels_for_insert_before_after_action(['_{', '}'], ['<Control><Shift>d'])
         self.set_accels_for_insert_before_after_action(['^{', '}'], ['<Control><Shift>u'])
 
         self.set_accels_for_insert_before_after_action(['\\begin{center}\n\\begin{circuitikz}\n\t \draw ','\n;\n\\end{circuitikz}\n\\end{center}'], ['<Control><shift>c'])
-        self.set_accels_for_insert_before_after_action(['() node[',',]{}'], ['<Control><shift>x'])
-        self.set_accels_for_insert_before_after_action(['(,) to [',']\n(,)'], ['<Control><shift>b'])
+        self.set_accels_for_insert_before_after_action([' node[',',]{}'], ['<Control><shift>x'])
+        self.set_accels_for_insert_before_after_action([' to [',']\n(•,•)'], ['<Control><shift>b'])
         self.set_accels_for_insert_before_after_action(['\\begin{tikzpicture}\centering\\begin{axis}\n\\addplot {','};\n\\end{axis}\\end{tikzpicture}'], ['<Control>p'])
         self.set_accels_for_insert_before_after_action(['[\ntitle={},\nxlabel={},\nylabel={},\nxmin=0, xmax=10,\nymin=0, ymax=10,\nxtick={0,2,4,6,8},\nytick={0,2,4,6,8},\nlegend pos=north east,\nymajorgrids=true,\ngrid style=dashed,\naxis lines=left',']'],['<Control><shift>p'])
         
-        
+        self.set_accels_for_insert_before_after_action(['\\documentclass[12pt]{article}\n\n\\usepackage{init}\n\n\\begin{document}\n\t\\maketitle\n\t\\newpage\n\t\\tableofcontents\n\t\\newpage\n\t\\section{','}\n\t•\n\\end{document}'], ['<Control><Shift>i'])
         
         self.set_accels_for_insert_symbol_action(['\\frac{•}{•}'], ['<Alt><Shift>f'])
         self.set_accels_for_insert_symbol_action(['\\left •'], ['<Control><Shift>l'])
         self.set_accels_for_insert_symbol_action(['\\right •'], ['<Control><Shift>r'])
-        self.set_accels_for_insert_symbol_action(['\\item •'], ['<Control><Shift>i'])
         self.set_accels_for_insert_symbol_action(['\\\\\n'], ['<Control>Return'])
-
+        
         self.create_and_add_shortcut('<Control>k', self.actions.toggle_comment)
         self.create_and_add_shortcut('<Control>quotedbl', self.shortcut_quotes)
 
